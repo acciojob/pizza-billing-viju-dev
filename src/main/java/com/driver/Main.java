8f516d671490794960f4304a97198010080f68dc
viju-dev <vijaydorugade20@gmail.com>
2023-01-17 01:18:23 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,13 +7,12 @@ public class Main {
     dp.addTakeaway();
     dp.addExtraCheese();
     dp.addTakeaway();
-    System.out.println(dp.getBill()); //640
+    System.out.println(dp.getBill());
 
     Pizza p = new Pizza(true);
     p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
-    System.out.println(p.getBill());//470
-
+    System.out.println(p.getBill());
   }
 }
